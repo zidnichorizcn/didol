a72b5b24707503d129d76d60be0e7f67b6b20803
didol <krb.bbkb@gmail.com>
2026-07-24 10:51:36 +0700

    Setup Gmail SMTP untuk reset password

--- django_project/settings.py
+++ django_project/settings.py
@@ -126,3 +126,11 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 LOGIN_REDIRECT_URL = "product_list"
 LOGOUT_REDIRECT_URL = "product_list"
+# Email settings
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "zidnichorizcn@gmail.com"
+EMAIL_HOST_PASSWORD = "sfwm vgeq qbrv trqn"
+DEFAULT_FROM_EMAIL = "Didol <zidnichorizcn@gmail.com>"
